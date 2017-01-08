4293f01e308e69207db908196258bde7cde39de7
Ben Foxall <benfoxall@gmail.com>
2017-01-08 09:15:49 +0000

    First pass - display some data

--- dist/hex.js
+++ dist/hex.js
@@ -1,11 +1,40 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global.contours = factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.Hex = factory());
 }(this, (function () { 'use strict';
 
-var hex = function () { return console.log('TODO: implement hex'); };
+var padZero = function (str, i) {
+  while(str.length < i)
+    { str = "0" + str; }
 
-return hex;
+  return str
+};
+
+
+var Hex = function Hex (buffer) {
+  this.buffer = buffer;
+  this.view = new DataView(buffer);
+
+  this.element = document.createElement('p');
+  this.element.style.fontFamily = 'monospace';
+  document.body.appendChild(this.element);
+};
+
+Hex.prototype.render = function render () {
+    var this$1 = this;
+
+  var items = this.buffer.byteLength;
+
+  this.element.textContent = '';
+
+  for (var i = 0; i < items; i++) {
+    this$1.element.textContent += padZero(this$1.view.getUint8(i).toString(16), 2) + ' ';
+  }
+
+  return this
+};
+
+return Hex;
 
 })));
--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
-const hex = () =>
-  console.log('TODO: implement hex')
+const padZero = (str, i) => {
+  while(str.length < i)
+    str = `0${str}`
 
-export default hex
+  return str
+}
+
+
+class Hex {
+
+  constructor (buffer) {
+    this.buffer = buffer
+    this.view = new DataView(buffer)
+
+    this.element = document.createElement('p')
+    this.element.style.fontFamily = 'monospace'
+    document.body.appendChild(this.element)
+  }
+
+  render() {
+    const items = this.buffer.byteLength
+
+    this.element.textContent = ''
+
+    for (var i = 0; i < items; i++) {
+      this.element.textContent += padZero(this.view.getUint8(i).toString(16), 2) + ' '
+    }
+
+    return this
+  }
+
+}
+
+export default Hex
